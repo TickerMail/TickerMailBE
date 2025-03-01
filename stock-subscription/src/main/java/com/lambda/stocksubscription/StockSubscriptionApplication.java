abd614508ac20dad3262bce24a0da094be270a4a
yeonsu <jack0810@kookmin.ac.kr>
2025-03-01 15:09:57 +0900

    first commit

---
+++ stock-subscription/src/main/java/com/lambda/stocksubscription/StockSubscriptionApplication.java
@@ -0,0 +1,13 @@
+package com.lambda.stocksubscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockSubscriptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockSubscriptionApplication.class, args);
+    }
+
+}
---
+++ stock-subscription/src/test/java/com/lambda/stocksubscription/StockSubscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambda.stocksubscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockSubscriptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
