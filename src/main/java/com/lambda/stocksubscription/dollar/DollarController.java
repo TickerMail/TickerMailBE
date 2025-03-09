8cece689d9de16ce9ece581ea05daba67f7a824c
yeonsu <jack0810@kookmin.ac.kr>
2025-03-09 17:14:27 +0900

    Internal: re route api contoller

--- src/main/java/com/lambda/stocksubscription/dollar/DollarController.java
+++ src/main/java/com/lambda/stocksubscription/dollar/DollarController.java
@@ -2,15 +2,17 @@ package com.lambda.stocksubscription.dollar;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/dollar")
 @RequiredArgsConstructor
 public class DollarController {
 
     private final DollarFetchService dollarFetchService;
 
-    @GetMapping("/test/dollar")
+    @GetMapping("/test")
     public void fetchDollar() throws Exception {
         dollarFetchService.fetchExchangeRates();
     }
